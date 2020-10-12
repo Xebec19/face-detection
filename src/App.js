a7c34372fd46bdc11ddb41a1fe9022455584743d
xebec19 <thakur190420@gmail.com>
2020-10-12 11:11:36 +0530

    Adding funtionality in the project

--- src/App.js
+++ src/App.js
@@ -5,26 +5,41 @@ import Logo from './components/Logo/Logo';
 import Particles from 'react-particles-js';
 import Rank from './components/Rank/Rank.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
-const parameter =   {polygon: {
-                      number: {
-                          value: 30,
-                          density: {
-                              enable: true,
-                              value_area: 800
-                          }
-                      }  
-                        
-                    }}
+
+const parameter =   {
+                      particles: {
+                                number: {
+                                        value: 30,
+                                        density: {
+                                                enable: true,
+                                                value_area: 800
+                                                  }
+                                        }  
+                                  }
+                    }
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => 
+  {
+    console.log(event.target.value);
+  }
+
   render(){
   return (
     <div className="App">
      <Particles className='particles' 
-                params={{parameter}} />
+                params={parameter} />
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm />
+     <ImageLinkForm onInputChange={this.onInputChange}/>
 {/*     <FaceRecognition />
 */} 
      </div>
